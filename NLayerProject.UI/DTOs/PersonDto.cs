b5f47f9f515279cc95d2cfa8597a6beea675fb03
nurcanozkann <nurcnozkn3@gmail.com>
2021-06-06 22:21:26 +0300

    UI layer and categoriescontroller created.

---
+++ NLayerProject.UI/DTOs/PersonDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NLayerProject.UI.DTOs
+{
+    public class PersonDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+    }
+}
---
+++ NLayerProject.UI/Filters/NotFoundFilter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NLayerProject.Core.Services;
+using NLayerProject.UI.DTOs;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayerProject.UI.Filters
+{
+    public class NotFoundFilter : ActionFilterAttribute
+    {
+        private readonly ICategoryService _categoryService;
+
+        public NotFoundFilter(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            int id = (int)context.ActionArguments.Values.FirstOrDefault();
+
+            var category = await _categoryService.GetByIdAsync(id);
+
+            if (category != null)
+            {
+                await next();
+            }
+            else
+            {
+                ErrorDto errorDto = new ErrorDto();
+
+                errorDto.Errors.Add($"id'si {id} olan kategori veritabanında bulunamadı.");
+
+                context.Result = new RedirectToActionResult("Error","Home", errorDto);
+            }
+        }
+    }
+}
